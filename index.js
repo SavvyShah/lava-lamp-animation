c212d55ed816b3755d5e3d06fc687184c9b00154
Shubham Shah <shubhamshahrising@gmail.com>
2023-05-18 19:10:10 +0530

    Compute the distance of a point from the centre of the circle and have a threshold value

--- index.js
+++ index.js
@@ -4,15 +4,23 @@ const STEP = 20;
 
 const gridPoints = [];
 
+class Circle {
+  constructor(x, y) {
+    this.centre = [x, y];
+  }
+  // 2D implicit function. Not to be accessed publicly
+  distance(x, y) {
+    return Math.sqrt((this.centre[0] - x) ** 2 + (this.centre[1] - y) ** 2);
+  }
+}
+
 // Add grid points from 0 to screen width and 0 to screen height
 function setup() {
   createCanvas(WIDTH, HEIGHT);
-  gridPoints;
 }
 
 function draw() {
   background(200);
-  stroke("purple");
   strokeWeight(3);
 
   const rows = Math.floor(WIDTH / STEP) + 1;
@@ -20,6 +28,10 @@ function draw() {
 
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
+      const circle = new Circle(150, 150);
+      const pointColor =
+        circle.distance(i * STEP, j * STEP) < 100 ? "purple" : "white";
+      stroke(pointColor);
       point(i * STEP, j * STEP);
     }
   }
