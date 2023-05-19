b947a5502970f76aa83c5e72af83f844e7314690
Shubham Shah <shubhamshahrising@gmail.com>
2023-05-19 08:38:25 +0530

    Sum total potential

--- index.js
+++ index.js
@@ -9,10 +9,24 @@ class Circle {
     this.centre = [x, y];
   }
   // 2D implicit function. Not to be accessed publicly
-  fieldForce(x, y) {
+  fieldPotential(x, y) {
     const r = Math.sqrt((this.centre[0] - x) ** 2 + (this.centre[1] - y) ** 2);
     return r > 0 ? 1 / r : 1;
   }
+}
+
+/**
+ *
+ * @param {number} x
+ * @param {number} y
+ * @param {Array<Circle>} circles
+ * @returns {number} sum of force
+ */
+function totalPotential(x, y, circles) {
+  return circles.reduce(
+    (initialValue, circle) => initialValue + circle.fieldPotential(x, y),
+    0
+  );
 }
 
 // Add grid points from 0 to screen width and 0 to screen height
@@ -30,11 +44,13 @@ function draw() {
     for (let j = 0; j < cols; j++) {
       const circle1 = new Circle(300, 300);
       const circle2 = new Circle(700, 300);
-      const fieldForce1 = circle1.fieldForce(i * STEP, j * STEP);
-      const fieldForce2 = circle2.fieldForce(i * STEP, j * STEP);
-      const fieldForce = fieldForce1 + fieldForce2;
+      const fieldPotential = totalPotential(i * STEP, j * STEP, [
+        circle1,
+        circle2,
+      ]);
 
-      const pointOpacity = fieldForce * 100 > 1 ? fieldForce ** 2 * 10 ** 6 : 0;
+      const pointOpacity =
+        fieldPotential * 100 > 1 ? fieldPotential ** 2 * 10 ** 6 : 0;
       strokeWeight(10);
       stroke(Math.random(), 100, 200, pointOpacity);
       point(i * STEP, j * STEP);
